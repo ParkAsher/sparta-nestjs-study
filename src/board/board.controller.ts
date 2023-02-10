8deaf35bf735795b7689b6a4fc2a3ae89035e295
ParkAsher <devasherpark@naver.com>
2023-02-11 00:36:32 +0900

    컨트롤러 작성

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  // service
+  constructor(private readonly boardService: BoardService) {}
+
+  // 게시물 목록을 가져오는 API
+  @Get('/articles')
+  getArticles() {
+    return this.boardService.getArticles();
+  }
+
+  // 게시물 상세보기
+  @Get('/articles/:id')
+  getArticleById() {
+    return this.boardService.getArticleById(id);
+  }
+
+  // 게시물 작성
+  @Post('/articles')
+  createArticle() {
+    return this.boardService.createArticle();
+  }
+
+  // 게시물 수정
+  @Put('/articles/:id')
+  updateArticle() {
+    return this.boardService.updateArticle(id);
+  }
+
+  // 게시물 삭제
+  @Delete('/articles/:id')
+  deleteArticle() {
+    return this.boardService.deleteArticle(id);
+  }
+}
