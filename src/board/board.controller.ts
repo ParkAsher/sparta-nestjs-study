c25d25f4d8a1f7b2372faceaa8e43afcedbac31d
ParkAsher <devasherpark@naver.com>
2023-02-11 16:39:39 +0900

    서비스 작성

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,38 +1,64 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+} from '@nestjs/common';
 import { BoardService } from './board.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { DeleteArticleDto } from './dto/delete-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Controller('board')
 export class BoardController {
-  // service
-  constructor(private readonly boardService: BoardService) {}
+    // service
+    constructor(private readonly boardService: BoardService) {}
 
-  // 게시물 목록을 가져오는 API
-  @Get('/articles')
-  getArticles() {
-    return this.boardService.getArticles();
-  }
+    // 게시물 목록을 가져오는 API
+    @Get('/articles')
+    getArticles() {
+        return this.boardService.getArticles();
+    }
 
-  // 게시물 상세보기
-  @Get('/articles/:id')
-  getArticleById() {
-    return this.boardService.getArticleById(id);
-  }
+    // 게시물 상세보기
+    @Get('/articles/:id')
+    getArticleById(@Param('id') articleId: number) {
+        return this.boardService.getArticleById(articleId);
+    }
 
-  // 게시물 작성
-  @Post('/articles')
-  createArticle() {
-    return this.boardService.createArticle();
-  }
+    // 게시물 작성
+    @Post('/articles')
+    createArticle(@Body() data: CreateArticleDto) {
+        return this.boardService.createArticle(
+            data.title,
+            data.content,
+            data.password,
+        );
+    }
 
-  // 게시물 수정
-  @Put('/articles/:id')
-  updateArticle() {
-    return this.boardService.updateArticle(id);
-  }
+    // 게시물 수정
+    @Put('/articles/:id')
+    updateArticle(
+        @Param('id') articleId: number,
+        @Body() data: UpdateArticleDto,
+    ) {
+        return this.boardService.updateArticle(
+            articleId,
+            data.title,
+            data.content,
+            data.password,
+        );
+    }
 
-  // 게시물 삭제
-  @Delete('/articles/:id')
-  deleteArticle() {
-    return this.boardService.deleteArticle(id);
-  }
+    // 게시물 삭제
+    @Delete('/articles/:id')
+    deleteArticle(
+        @Param('id') articleId: number,
+        @Body() data: DeleteArticleDto,
+    ) {
+        return this.boardService.deleteArticle(articleId, data.password);
+    }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from '@nestjs/common';
+import _ from 'lodash';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+    private articles = [];
+
+    // 게시글 비밀번호 저장 객체
+    private articlePasswords = new Map();
+
+    getArticles() {
+        return this.articles;
+    }
+
+    getArticleById(id: number) {
+        return this.articles.find((article) => {
+            return article.id === id;
+        });
+    }
+
+    createArticle(title: string, content: string, password: string) {
+        const articleId = this.articles.length + 1;
+
+        this.articles.push({
+            id: articleId,
+            title,
+            content,
+        });
+
+        this.articlePasswords.set(articleId, password);
+
+        return articleId;
+    }
+
+    updateArticle(
+        id: number,
+        title: string,
+        content: string,
+        password: string,
+    ) {
+        // 비밀번호 확인
+        if (this.articlePasswords.get(id) !== password) {
+            throw new UnauthorizedException(
+                `Article password is not correct. id: ${id}`,
+            );
+        }
+
+        const article = this.getArticleById(id);
+        if (_.isNil(article)) {
+            throw new NotFoundException(`Article not found. id: ${id}`);
+        }
+
+        article.title = title;
+        article.content = content;
+    }
+
+    deleteArticle(id: number, password: string) {
+        if (this.articlePasswords.get(id) !== password) {
+            throw new UnauthorizedException(
+                `Article password is not correct. id: ${id}`,
+            );
+        }
+
+        this.articles = this.articles.filter((article) => article.id !== id);
+    }
+}
